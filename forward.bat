eb3f77b315a9f0527e83aaf502a7ce5b82ea9337
РќРёРєРёС‚Р° <nikita@tg-text.ru>
2026-02-13 16:59:23 +0300

    Update: 2026-02-13 16:59:23

--- forward.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Батник-обертка для запуска forward.ps1 с обходом политики выполнения
-powershell -ExecutionPolicy Bypass -File "%~dp0forward.ps1"
-pause
-
--- rollback.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Батник-обертка для запуска rollback.ps1 с обходом политики выполнения
-powershell -ExecutionPolicy Bypass -File "%~dp0rollback.ps1"
-pause
-
